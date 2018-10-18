97c70843c60d9d677aca3f0fa14ee1701add086c
Sam Lester <sam@inktrap.co.uk>
2018-10-18 20:51:38 +0100

    Added general.php and .env.example

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# craft-setup-files
+# Craft setup files
+Our standard Craft 3 config. Used when converting a Baseline project to work with Craft.
---
+++ general.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see craft\config\GeneralConfig
+ */
+
+return [
+  // Global settings
+  '*' => [
+    // Default Week Start Day (0 = Sunday, 1 = Monday...)
+    'defaultWeekStartDay' => 0,
+
+    // Enable CSRF Protection (recommended)
+    'enableCsrfProtection' => true,
+
+    // Whether generated URLs should omit "index.php"
+    'omitScriptNameInUrls' => true,
+
+    // Control Panel trigger word
+    'cpTrigger' => 'admin',
+
+    // The secure key Craft will use for hashing and encrypting data
+    'securityKey' => getenv('SECURITY_KEY'),
+
+    // Time and date settings
+    'defaultWeekStartDay' => 1,
+    'timezone' => 'Europe/London',
+
+    // Base site URL
+    'siteUrl' => getenv('SITE_URL'),
+
+    // Image transform settings
+    'generateTransformsBeforePageLoad' => true,
+    'defaultImageQuality' => 75,
+
+    // Search settings
+    'defaultSearchTermOptions' => array(
+      'subLeft' => true,
+      'subRight' => true,
+    ),
+
+    // Aliases parsed in sites’ settings, volumes’ settings, and Local volumes’ settings
+    'aliases' => [
+      '@assetDirectory' => getenv('ASSET_DIRECTORY'),
+      '@uploadsDirectory' => getenv('UPLOADS_DIRECTORY'),
+    ],
+
+    // Site-specific config settings
+    'custom' => [
+      'staticAssetsVersion' => 1,
+    ]
+  ],
+
+  // Development environment settings
+  'dev' => [
+    // Dev mode
+    'devMode' => true,
+
+    // Craft template caching
+    'enableTemplateCaching' => false,
+
+    // Site-specific config settings
+    'custom' => [
+      'staticAssetsVersion' => time(),
+    ]
+  ],
+
+  // Staging environment settings
+  'staging' => [
+    // Disable automated updates
+    'allowUpdates' => false,
+
+    // Craft template caching
+    'enableTemplateCaching' => true,
+    'cacheDuration' => 'PT30M',
+  ],
+
+  // Production environment settings
+  'production' => [
+    // Disable automated updates
+    'allowUpdates' => false,
+
+    // Craft template caching
+    'enableTemplateCaching' => true,
+    'cacheDuration' => 'P1W',
+  ],
+];
